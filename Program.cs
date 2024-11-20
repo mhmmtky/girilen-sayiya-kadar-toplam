2df47d95467df8e7acf90a09af7a27139c4132d6
mhmmtky <mhmmtky78@gmail.com>
2024-11-20 23:38:25 +0300

    toplam

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sayiToplami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+
+            for (int i = 0; i<=sayi; i++)
+            {
+                toplam = toplam + i;
+            }
+            Console.WriteLine(toplam);
+            Console.ReadKey();
+        }
+    }
+}
